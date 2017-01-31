9e81b46a8c442e8c1e2abbd02f6d8851d0778f74
Holy-Knight <vnycall74@naver.com>
2017-01-31 17:48:18 +0900

    Second
    Second

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public int money = 1000;
+    public int[] item = new int[10];
+
+    public void RefreshMoney(int m)
+    {
+        money -= m;
+
+        Debug.Log("RefreshMoney : " + money);
+    }
+}
